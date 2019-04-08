e53bd1be7176473e0172ea93407b3fba64246831
Duy Nguyen <38705018+hydrophyl@users.noreply.github.com>
2019-04-08 12:38:30 +0000

    Add exc14.py

---
+++ LPTHW/exc14.py
@@ -0,0 +1,39 @@
+""" from sys import argv
+
+script, user_name = argv
+prompt = '>.< '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f""""""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice. 
+"""
+
+from sys import argv
+
+script, user_name = argv
+prompt = '>,< >.< '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("Now i have you ask you some questions.")
+print("What's your girlfriends name? ")
+girlfriend = input(prompt)
+
+print("Where do you live?")
+live = input(prompt)
+
+print("How old are you?")
+old = input(prompt)
+
+print(f"")
